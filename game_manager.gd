0b74c472e4426bcee3a614b0406b7fae129f17cd
Karen McMullan <kpmcmullan@gmail.com>
2024-07-15 18:15:18 -0700

    added P for pausing and unpausing

--- game_manager.gd
+++ game_manager.gd
@@ -1,7 +1,6 @@
 extends Node
 
 func _input(event : InputEvent):
-	if(event.is_action_pressed("ui_cancel")):
-		print("running cancel event")
+	if(event.is_action_pressed("ui_cancel") or event.is_action_pressed("pause")):
 		var current_value : bool = get_tree().paused
 		get_tree().paused = !current_value
