2e1378fa873a45f07359e4b1c9ce471a7cb07897
gabrielcornish <58050002+gabrielcornish@users.noreply.github.com>
2024-07-20 11:23:32 -0700

    Created global scene manager autoload

---
+++ Globals/SceneManager.gd
@@ -0,0 +1,32 @@
+extends Node
+class_name NSceneManager
+
+@export var Scenes : Dictionary = {}
+
+var m_CurrentSceneAlias : String = ""
+
+# Find the initial scene as defined in the project settings
+func _ready() -> void:
+	var mainScene : StringName = ProjectSettings.get_setting("application/run/main_scene")
+	m_CurrentSceneAlias = Scenes.find_key(mainScene)
+
+func AddScene(sceneAlias : String, scenePath : String) -> void:
+	Scenes[sceneAlias] = scenePath
+	
+func RemoveScene(sceneAlias : String) -> void:
+	Scenes.erase(sceneAlias)
+	
+func SwitchScene(sceneAlias : String) -> void:
+	get_tree().change_scene_to_file(Scenes[sceneAlias])
+
+func RestartScene() -> void:
+	get_tree().reload_current_scene()
+	
+func QuitGame() -> void:
+	get_tree().quit()
+	
+func GetSceneCount() -> int:
+	return Scenes.size()
+	
+func GetCurrentSceneAlias() -> String:
+	return m_CurrentSceneAlias
