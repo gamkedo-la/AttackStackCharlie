d3422d4ea1dbad114a179ceb6c01088c8e94be58
Karen McMullan <kpmcmullan@gmail.com>
2024-07-23 16:16:33 -0700

    partial migration of pause menu into scene manager

--- Globals/SceneManager.gd
+++ Globals/SceneManager.gd
@@ -5,6 +5,18 @@ class_name NSceneManager
 
 var m_CurrentSceneAlias : String = ""
 
+# migrating game_manager script stuff related to pause menu into here
+signal toggle_game_paused(is_paused : bool)
+
+# migrating game_manager script stuff related to pause menu into here
+var game_paused : bool = false:
+	get:
+		return game_paused
+	set(value):
+		game_paused = value
+		get_tree().paused = game_paused
+		emit_signal("toggle_game_paused", game_paused)
+
 # Find the initial scene as defined in the project settings
 func _ready() -> void:
 	var mainScene : StringName = ProjectSettings.get_setting("application/run/main_scene")
@@ -30,3 +42,8 @@ func GetSceneCount() -> int:
 	
 func GetCurrentSceneAlias() -> String:
 	return m_CurrentSceneAlias
+
+# migrating game_manager script stuff related to pause menu into here
+func _input(event : InputEvent):
+	if(event.is_action_pressed("ui_cancel") or event.is_action_pressed("pause")):
+		game_paused = !game_paused
--- UI/PauseMenu/pause_menu.gd
+++ UI/PauseMenu/pause_menu.gd
@@ -1,27 +1,27 @@
 extends Control
 
-@export var game_manager : GameManager
+@export var scene_manager : NSceneManager
 
 # Called when the node enters the scene tree for the first time.
 func _ready():
 	hide()
-	game_manager.connect("toggle_game_paused", _on_game_manager_toggle_game_paused)
+	scene_manager.connect("toggle_game_paused", _on_scene_manager_toggle_game_paused)
 
-# Called every frame. 'delta' is the elapsed time since the previous frame.
-func _process(delta):
-	pass
-
-func _on_game_manager_toggle_game_paused(is_paused : bool):
+func _on_scene_manager_toggle_game_paused(is_paused : bool):
 	if(is_paused):
+		print("Show called")
 		show()
 	else:
+		print("Hide called")
 		hide()
 		
-
 func _on_resume_btn_pressed():
-	game_manager.game_paused = false
+	print("resume button pressed")
+	scene_manager.game_paused = false
+
+func _on_main_menu_btn_pressed():
+	print("Add go to main menu here")
 
 func _on_exit_btn_pressed():
-	print("quit button pressed")
-	get_tree().quit()
+	scene_manager.QuitGame()
 
