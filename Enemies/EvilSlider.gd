b54c94ee7e927b1e3b2e06957a90c376c43917e3
anthonyhenry <anthonyh1497@gmail.com>
2024-08-03 22:15:15 -0700

    Enemies self destruct if they go off screen

--- Enemies/EvilSlider.gd
+++ Enemies/EvilSlider.gd
@@ -6,3 +6,7 @@ func _ready():
 func _process(delta):
 	position += moveDir * delta * ENEMY_SPEED
 	pass
+
+func _on_visible_on_screen_notifier_2d_screen_exited():
+	queue_free()
+	pass # Replace with function body.
