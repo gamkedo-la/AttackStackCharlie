6e6a9a41d2c90ba01e6724789a00cd35009d6565
Chris DeLeon <chris@gamkedo.com>
2024-08-11 13:47:27 -0400

    Trying another approach, centralizing explosion management (still WIP / no chain reactions yet)

--- Enemies/Enemy.gd
+++ Enemies/Enemy.gd
@@ -3,21 +3,28 @@ var moveDir = Vector2.ZERO;
 const ENEMY_SPEED = 50
 
 var exploded = false
-var explosion_radius = 150
-var explosion_area: Area2D = null 
+var explosion_radius = 500
+
+signal enemy_exploded(position, radius)
 
 # Called when the node enters the scene tree for the first time.
 func _ready():
+	add_to_group("enemies")
 	setDirection(Vector2.RIGHT)
-	explosion_area = Area2D.new()
+	var explosion_manager = get_node_or_null("/root/ExplosionManager")
+	if explosion_manager:
+		connect("enemy_exploded", Callable(explosion_manager, "trigger_explosion_at_position"))
+	else:
+		print("ERROR: ExplosionManager not found :(")
+	var explosion_area = Area2D.new()
 	var collision_shape = CollisionShape2D.new()
 	collision_shape.shape = CircleShape2D.new()
 	collision_shape.shape.radius = explosion_radius
 	explosion_area.add_child(collision_shape)
-	explosion_area.connect("area_entered", Callable(self, "_on_explosion_area_entered"))
 	add_child(explosion_area)
-	explosion_area.name = "ExplosionArea"
-	explosion_area.set_collision_mask_bit(0, false)
+	explosion_area.set_collision_layer_bit(1, true)
+	explosion_area.set_collision_mask_bit(1, true)
+	explosion_area.add_to_group("explosion_areas")
 	pass
 
 func setDirection(direction: Vector2):
@@ -32,15 +39,9 @@ func _on_projectile_detector_area_entered(area):
 		explode()
 
 func explode():
-	exploded = true
+	print("enemy exploding")
+	emit_signal("enemy_exploded", global_position, explosion_radius)
 	queue_free()
-	if explosion_area and is_instance_valid(explosion_area):
-		var overlapping_areas = explosion_area.get_overlapping_areas()
-		for area in overlapping_areas:
-			if area.is_in_group("hitbox") and area is Enemy and not area.exploded:
-				area.get_parent().call_deferred("explode_with_delay")
-	else:
-		print("DEBUG: Explosion area not found or is invalid.")
 
 func explode_with_delay():
 	var timer = get_tree().create_timer(0.1)
--- Enemies/EvilSlider.gd
+++ Enemies/EvilSlider.gd
@@ -9,4 +9,4 @@ func _process(delta):
 
 func _on_visible_on_screen_notifier_2d_screen_exited():
 	queue_free()
-	pass # Replace with function body.
+	pass
