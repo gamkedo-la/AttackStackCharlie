7914a70a31e36486c25f61c4ec8b5de3ee34c4c7
Chris DeLeon <chris@gamkedo.com>
2024-08-18 02:35:08 -0400

    Chain reaction explosions working for enemies

--- Enemies/Enemy.gd
+++ Enemies/Enemy.gd
@@ -5,9 +5,9 @@ const ENEMY_SPEED = 50
 var exploded = false
 var explosion_radius = 500
 
+var explosion_scene = preload("res://Explosions/EnemyBlast.tscn")
 signal enemy_exploded(position, radius)
 
-# Called when the node enters the scene tree for the first time.
 func _ready():
 	add_to_group("enemies")
 	setDirection(Vector2.RIGHT)
@@ -20,7 +20,12 @@ func _process(delta):
 	position += moveDir * delta * ENEMY_SPEED
 	pass
 
+func destroy():
+	var explosion = explosion_scene.instantiate()
+	explosion.position = position
+	get_tree().root.call_deferred("add_child", explosion) 
+	queue_free()
+
 func _on_projectile_detector_area_entered(area):
 	if area.is_in_group("bullet"):
-		print("enemy exploding")
-		queue_free()
+		destroy()
---
+++ Explosions/EnemyBlast.gd
@@ -0,0 +1,33 @@
+extends Area2D
+
+var max_scale = 4
+var growth_rate = 0.2
+var active = true
+
+func _ready():
+	scale = Vector2(0.1, 0.1)
+	self.connect("area_entered", Callable(self, "_on_Area2D_area_entered"))
+
+func _process(delta):
+	if scale.x < max_scale and active:
+		scale += Vector2(growth_rate, growth_rate)
+		update_collision_shape()
+	else:
+		queue_free()
+
+func update_collision_shape():
+	if $CollisionShape2D.shape is CircleShape2D:
+		$CollisionShape2D.shape.radius = 10.0 * scale.x
+
+func _on_Area2D_area_entered(area):
+	# print("Area entered: ", area.name) 
+	var areaParent = area.get_parent()
+	if areaParent.is_in_group("enemies") and active:
+		# print("Destroying enemy: ", areaParent.name)
+		if areaParent.has_method("destroy"):
+			areaParent.destroy()
+		else:
+			print("No destroy method found")
+
+func deactivate():
+	active = false
