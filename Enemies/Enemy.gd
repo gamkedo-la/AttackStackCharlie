46b3230614b5c03bea7534d0ffa973d3a20447fb
Chris DeLeon <chris@gamkedo.com>
2024-08-18 00:47:49 -0400

    Removing my previous explosion attempt to try the newer way

--- Enemies/Enemy.gd
+++ Enemies/Enemy.gd
@@ -11,20 +11,6 @@ signal enemy_exploded(position, radius)
 func _ready():
 	add_to_group("enemies")
 	setDirection(Vector2.RIGHT)
-	var explosion_manager = get_node_or_null("/root/ExplosionManager")
-	if explosion_manager:
-		connect("enemy_exploded", Callable(explosion_manager, "trigger_explosion_at_position"))
-	else:
-		print("ERROR: ExplosionManager not found :(")
-	var explosion_area = Area2D.new()
-	var collision_shape = CollisionShape2D.new()
-	collision_shape.shape = CircleShape2D.new()
-	collision_shape.shape.radius = explosion_radius
-	explosion_area.add_child(collision_shape)
-	add_child(explosion_area)
-	explosion_area.set_collision_layer_bit(1, true)
-	explosion_area.set_collision_mask_bit(1, true)
-	explosion_area.add_to_group("explosion_areas")
 	pass
 
 func setDirection(direction: Vector2):
@@ -35,15 +21,6 @@ func _process(delta):
 	pass
 
 func _on_projectile_detector_area_entered(area):
-	if area.is_in_group("bullet") and not exploded:
-		explode()
-
-func explode():
-	print("enemy exploding")
-	emit_signal("enemy_exploded", global_position, explosion_radius)
-	queue_free()
-
-func explode_with_delay():
-	var timer = get_tree().create_timer(0.1)
-	await timer.timeout
-	explode()
+	if area.is_in_group("bullet"):
+		print("enemy exploding")
+		queue_free()
