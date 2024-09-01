f98d0f4a5af37746ed7ce66b6e6aaa85df910ff0
Chris DeLeon <chris@gamkedo.com>
2024-09-01 15:46:52 -0400

    Round name at start now input at top level of each stage script

--- Levels/MainPlay.gd
+++ Levels/MainPlay.gd
@@ -1,5 +1,6 @@
 extends Node2D
 var RoundTitle = preload("res://UI/HUD/round_title.tscn")
+@export var LevelName = "StageName";
 
 # Called when the node enters the scene tree for the first time.
 func _ready():
--- UI/HUD/round_title.gd
+++ UI/HUD/round_title.gd
@@ -2,11 +2,10 @@ extends Node
 
 @onready var message = get_node("Message")
 @onready var tween = create_tween()
-var round = 1
 
 # Called when the node enters the scene tree for the first time.
 func _ready():
-	message.text = "Round %s" % round
+	message.text = get_tree().current_scene.LevelName
 	tween.tween_property(self, "modulate", Color.GRAY, 1).set_trans(Tween.TRANS_SINE)
 	tween.tween_callback(self.queue_free)
 
