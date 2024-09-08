cabb678a7aa7f851c4ffdd94ddc9e77eb8b01990
Michael Tanner <66394061+michael-tanner-coder@users.noreply.github.com>
2024-09-08 00:50:59 -0500

    100 second timer that will restart the level when it reaches 0

--- Levels/MainPlay.gd
+++ Levels/MainPlay.gd
@@ -1,6 +1,9 @@
 extends Node2D
 var RoundTitle = preload("res://UI/HUD/round_title.tscn")
 @export var LevelName = "StageName";
+var timerCount: int = 100;
+@onready var timer = get_tree().current_scene.get_node("EveryLevelReusedStuff/Timer");
+@onready var countdown = get_tree().current_scene.get_node("EveryLevelReusedStuff/Countdown");
 
 # Called when the node enters the scene tree for the first time.
 func _ready():
@@ -10,4 +13,9 @@ func _ready():
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta):
+	timer.text = "TIME: " + str(snapped(countdown.time_left, 1));
+	
+	if (countdown.time_left <= 0):
+		SceneManager.RestartScene();
+		
 	pass
