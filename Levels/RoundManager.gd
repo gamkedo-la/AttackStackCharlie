9dcfaa321eb709ef38237a2618f5dbeff7421fcd
Michael Tanner <66394061+michael-tanner-coder@users.noreply.github.com>
2024-09-21 21:47:52 -0500

    displaying enemy kill counter and victory text

--- Levels/RoundManager.gd
+++ Levels/RoundManager.gd
@@ -3,15 +3,20 @@ extends Node
 # Add a variable to count defeated enemies.
 var defeated_enemies = 0
 var level_goal = 10  # This can be set in the Inspector.
+@onready var enemy_kill_counter = $EnemyKillCounter
+@onready var victory_text = $VictoryText
 
 func _ready():
+	victory_text.visible = false;
+	enemy_kill_counter.text = str(defeated_enemies) + "/" + str(level_goal);
 	pass
 
 func _on_enemy_defeated():
-	# print("enemy removal signal receieved")
 	defeated_enemies += 1
-	print(str(defeated_enemies) + " enemies defeated")
-	# $UI/EnemyCounterLabel.text = str(defeated_enemies)
+	
+	enemy_kill_counter.text = str(defeated_enemies) + "/" + str(level_goal);
+	
 	if defeated_enemies >= level_goal:
-		print("Level won!")
+		SceneManager.game_paused = true;
+		victory_text.visible = true;
 	pass
