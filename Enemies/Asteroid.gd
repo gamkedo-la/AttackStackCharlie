585c652c77b9aa2d03222fa8edfdd347624556e4
Rodrigo Bonzerr S. Lopez <rodprogramdev@gmail.com>
2024-09-28 22:32:05 +0800

    Test how to move asteroid twrds Playerdrone - WIP
    Just

--- Enemies/Asteroid.gd
+++ Enemies/Asteroid.gd
@@ -1,6 +1,23 @@
 extends CharacterBody2D
 
 var rotation_speed = 90
+@export var speed: float = 100.0
+@export var detection_range: float = 200.0
+
+@export var player: Node2D
+func _ready():
+	player = get_tree().get_root().get_node("MainPlay/Playerdrone")
 
 func _process(delta):
 	rotation_degrees += rotation_speed * delta
+	
+	if player: 
+		var dir_to_player = player.global_position - global_position
+		var dis_to_player = dir_to_player.length()
+		
+		if dis_to_player <= detection_range:
+			move_towards_player(dir_to_player.normalized())
+			
+func move_towards_player(direction: Vector2):
+	velocity = direction * speed
+	move_and_slide()
