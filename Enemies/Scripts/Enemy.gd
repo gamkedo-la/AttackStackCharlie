51efd9b9dc7e0b1cfcdb61b6a64cee622afaad57
Chris DeLeon <chris@gamkedo.com>
2024-09-28 21:04:30 -0400

    Powerups now spawn from enemies. Don't yet vanish after timer

--- Enemies/Scripts/Enemy.gd
+++ Enemies/Scripts/Enemy.gd
@@ -10,7 +10,18 @@ signal enemy_exploded(position, radius)
 
 signal enemy_defeated()
 
+var powerup_paths = [
+    "res://Powerups/UpgradeShot_Range.tscn",
+    "res://Powerups/UpgradeShot_ROF.tscn",
+    "res://Powerups/UpgradeShot_Split.tscn",
+    "res://Powerups/Upgrade_AddDrone.tscn"
+]
+
+var powerup_spawn_counts = {}
+
 func _ready():
+	for path in powerup_paths:
+		powerup_spawn_counts[path] = 0
 	add_to_group("enemies")
 	var roundManagerNode = get_tree().current_scene.get_node("EveryLevelReusedStuff")	
 	if roundManagerNode:
@@ -30,6 +41,7 @@ func _process(delta):
 
 
 func destroy():
+	try_spawn_powerup()
 	var explosion = explosion_scene.instantiate()
 	explosion.position = position
 	get_tree().root.call_deferred("add_child", explosion) 
@@ -40,3 +52,36 @@ func destroy():
 func _on_projectile_detector_area_entered(area):
 	if area.is_in_group("bullet"):
 		destroy()
+
+func try_spawn_powerup():
+	if randf() < 0.25:
+		spawn_powerup()
+
+func sum_array(values):
+	var total = 0.0
+	for value in values:
+		total += value
+	return total
+
+func spawn_powerup():
+	var total = sum_array(powerup_spawn_counts.values()) + 1
+	var weights = []
+	for path in powerup_paths:
+		var count = powerup_spawn_counts[path]
+		weights.append(1.0 / (count + 1))
+	var weight_sum = sum_array(weights)
+	for i in range(weights.size()):
+		weights[i] = weights[i] / weight_sum
+	var random_pick = randf()
+	var accum = 0.0
+	var index = 0
+	for weight in weights:
+		accum += weight
+		if random_pick < accum:
+			break
+		index += 1
+	var powerup_scene = load(powerup_paths[index])
+	var powerup_instance = powerup_scene.instantiate()
+	get_parent().add_child(powerup_instance)
+	powerup_instance.global_position = global_position
+	powerup_spawn_counts[powerup_paths[index]] += 1 # so we can later lower odds of repeats
