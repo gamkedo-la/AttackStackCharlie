49e262a6eca07ad0d273e3a3cb8a153460e7a62b
Chris DeLeon <chris@gamkedo.com>
2024-09-28 21:31:03 -0400

    Powerups flicker then vanish

--- Powerups/Powerup.gd
+++ Powerups/Powerup.gd
@@ -2,9 +2,21 @@ extends Area2D
 
 const COLLECTION_VFX = preload("res://Feedback VFX/collection_feedback.tscn")
 
-func _ready():
-	# to do: set up timer to flash and vanish
-	pass
+const POWERUP_LIFETIME = 1.75
+const FLASHING_TIME = 0.75 # total, not additive
+const FLASH_TOGGLE_TIME = 0.1 # adjust to change flash rate
+
+var elapsed_time = 0.0
+var last_toggle_time = 0.0
+
+func _process(delta):
+	elapsed_time += delta
+	if elapsed_time >= POWERUP_LIFETIME-FLASHING_TIME:
+		if elapsed_time - last_toggle_time >= FLASH_TOGGLE_TIME:
+			visible = !visible
+			last_toggle_time = elapsed_time
+	if elapsed_time >= POWERUP_LIFETIME:
+		queue_free()
 
 func _on_body_entered(body):
 	if collected(body):
