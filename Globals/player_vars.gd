fb9aa75f14a3dec15630e2aeb788837dd914f1f8
Michael Tanner <66394061+michael-tanner-coder@users.noreply.github.com>
2024-10-12 22:16:14 -0500

    player vars to track stats for the summary screen

--- Globals/player_vars.gd
+++ Globals/player_vars.gd
@@ -3,7 +3,13 @@ extends Node
 var player_max_health: int = 3
 var player_health: int = 3
 var player_max_shield: int = 6
-var player_shield: int = 0
+var player_shield: int = 0;
+
+# stats for summary screen
+var shots_fired: int = 10;
+var time_in_level: int = 0;
+var hits_taken: int = 0;
+var powerups_collected: int = 0;
 
 # Called when the node enters the scene tree for the first time.
 func _ready():
