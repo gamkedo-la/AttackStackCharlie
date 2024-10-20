f85ae3333bcb3634ed83b996e86114cc8d574298
Chris DeLeon <chris@gamkedo.com>
2024-10-20 12:20:22 -0400

    Ongoing work cleaning up circler, turret aim working but WIP enemy circle strafe

--- Enemies/Scripts/EvilCircler.gd
+++ Enemies/Scripts/EvilCircler.gd
@@ -1,7 +1,7 @@
 extends "res://Enemies/Scripts/Enemy.gd"
 
 const ROTATE_SPEED = 0.2
-const CIRCLE_DISTANCE = 500
+const CIRCLE_DISTANCE = 150
 
 var circlePercent = 0
 var inRangeOfTarget
@@ -15,32 +15,26 @@ func _process(delta):
 	var rotationAmount = circlePercent * TAU
 	var distToPlayer = global_position.distance_to(targetPos)
 	var wasPos = global_position;
+	
+	circlePercent += ROTATE_SPEED * delta
+	if circlePercent > 1.0:
+		circlePercent -= 1.0
+
 	if distToPlayer <= CIRCLE_DISTANCE:
 		if inRangeOfTarget == false:
 			inRangeOfTarget = true
 			var targetSpotOnUnitCircle = (global_position - targetPos).normalized()
-			# print("Vector: ")
-			# print(targetSpotOnUnitCircle)
 			rotationAmount = targetSpotOnUnitCircle.angle()
-			# print("Radians: ")
-			# print(rotationAmount)
 		var rotDir = -1;
 		if cwOrbit:
 			rotDir = 1
-		targetPos.x += cos(rotationAmount + rotDir*PI/2) * CIRCLE_DISTANCE
-		targetPos.y += sin(rotationAmount + rotDir*PI/2) * CIRCLE_DISTANCE
-		global_position += (targetPos - global_position).normalized() * delta * ENEMY_SPEED * 5
+		global_position.x = targetPos.x + cos(rotationAmount + rotDir*PI/2) * CIRCLE_DISTANCE
+		global_position.y = targetPos.y + sin(rotationAmount + rotDir*PI/2) * CIRCLE_DISTANCE
 	else:
 		# print("outofrange")
-		inRangeOfTarget = false
 		global_position += (targetPos - global_position).normalized() * delta * ENEMY_SPEED * 5
 	var moveAng = atan2(global_position.y-wasPos.y,global_position.x-wasPos.x)
-	rotation = moveAng
-	
-
-	circlePercent += ROTATE_SPEED * delta
-	if circlePercent > TAU:
-		circlePercent -= TAU
+	rotation = moveAng	
 	pass
 
 func _on_visible_on_screen_notifier_2d_screen_exited():
