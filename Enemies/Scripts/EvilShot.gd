c01d981d6ba1893ee945cba1b5b7de21d9d97b03
Chris DeLeon <chris@gamkedo.com>
2024-10-20 10:47:53 -0400

    Added enemy projectile type, not yet being fired

--- Enemies/Scripts/EvilShot.gd
+++ Enemies/Scripts/EvilShot.gd
@@ -1,14 +1,12 @@
 extends "res://Enemies/Scripts/Enemy.gd"
 
-@export var speed: float = 10.0
-
 func _ready():
 	super._ready() # run the parent enemy init
 	pass
 
 func _process(delta):
 	moveDir = Vector2(ENEMY_SPEED, 0).rotated(rotation)
-	position += moveDir * delta * speed
+	position += moveDir * delta
 	pass
 
 func _on_visible_on_screen_notifier_2d_screen_exited():
