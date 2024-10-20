a730a766dcfa5b9a445604830779f3d815d1d786
Chris DeLeon <chris@gamkedo.com>
2024-10-20 12:43:41 -0400

    Circler behavior fixed, also randomized their orbit range/distance

--- Enemies/Scripts/EvilCircler.gd
+++ Enemies/Scripts/EvilCircler.gd
@@ -1,40 +1,47 @@
 extends "res://Enemies/Scripts/Enemy.gd"
 
-const ROTATE_SPEED = 0.2
-const CIRCLE_DISTANCE = 150
+const ROTATE_SPEED_MAX = 0.1
+const ROTATE_SPEED_MIN = 0.225
+const CIRCLE_DISTANCE_MAX = 270
+const CIRCLE_DISTANCE_MIN = 125
 
 var circlePercent = 0
-var inRangeOfTarget
+var myOrbitDist = 0
+var myOrbitRate = 0
+var inRangeOfTarget = false
 var cwOrbit = false;
 
 func _ready():
 	super._ready() # run the parent enemy init
 	cwOrbit = randi_range(0,10) < 5
+	myOrbitDist = randf_range(CIRCLE_DISTANCE_MIN,CIRCLE_DISTANCE_MAX)
+	myOrbitRate = randf_range(ROTATE_SPEED_MIN,ROTATE_SPEED_MAX)
 
 func _process(delta):
 	var rotationAmount = circlePercent * TAU
 	var distToPlayer = global_position.distance_to(targetPos)
 	var wasPos = global_position;
 	
-	circlePercent += ROTATE_SPEED * delta
-	if circlePercent > 1.0:
-		circlePercent -= 1.0
-
-	if distToPlayer <= CIRCLE_DISTANCE:
+	if inRangeOfTarget || distToPlayer <= myOrbitDist:
 		if inRangeOfTarget == false:
 			inRangeOfTarget = true
 			var targetSpotOnUnitCircle = (global_position - targetPos).normalized()
-			rotationAmount = targetSpotOnUnitCircle.angle()
+			var initialAngle = targetSpotOnUnitCircle.angle()
+			circlePercent = initialAngle / TAU
+			if cwOrbit:
+				circlePercent *= -1
 		var rotDir = -1;
 		if cwOrbit:
 			rotDir = 1
-		global_position.x = targetPos.x + cos(rotationAmount + rotDir*PI/2) * CIRCLE_DISTANCE
-		global_position.y = targetPos.y + sin(rotationAmount + rotDir*PI/2) * CIRCLE_DISTANCE
+		circlePercent += myOrbitRate * delta * rotDir
+		global_position.x = targetPos.x + cos(rotationAmount) * myOrbitDist
+		global_position.y = targetPos.y + sin(rotationAmount) * myOrbitDist
+		rotation = rotationAmount + rotDir * PI/2
 	else:
 		# print("outofrange")
 		global_position += (targetPos - global_position).normalized() * delta * ENEMY_SPEED * 5
-	var moveAng = atan2(global_position.y-wasPos.y,global_position.x-wasPos.x)
-	rotation = moveAng	
+		var moveAng = atan2(global_position.y-wasPos.y,global_position.x-wasPos.x)
+		rotation = moveAng
 	pass
 
 func _on_visible_on_screen_notifier_2d_screen_exited():
