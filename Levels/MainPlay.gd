3ace2d48b05e145012249539c57d16d68120d3ca
Dan Dela Rosa <dandelarosadeveloper@gmail.com>
2024-12-07 16:38:19 -0500

    Add health label

--- Levels/MainPlay.gd
+++ Levels/MainPlay.gd
@@ -4,18 +4,24 @@ var RoundTitle = preload("res://UI/HUD/round_title.tscn")
 var timerCount: int = 100;
 @onready var timer = get_tree().current_scene.get_node("EveryLevelReusedStuff/Timer");
 @onready var countdown = get_tree().current_scene.get_node("EveryLevelReusedStuff/Countdown");
+@onready var health = get_tree().current_scene.get_node("EveryLevelReusedStuff/Health");
 
 # Called when the node enters the scene tree for the first time.
 func _ready():
 	PlayerVars.player_health = PlayerVars.player_max_health
+	updateHealthDisplay();
 	var roundTitle = RoundTitle.instantiate()
 	add_child(roundTitle)
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta):
 	timer.text = "TIME: " + str(snapped(countdown.time_left, 1));
+	updateHealthDisplay();
 	
 	if (countdown.time_left <= 0):
 		SceneManager.RestartScene();
 		
 	pass
+
+func updateHealthDisplay():
+	health.text = "HEALTH: " + str(PlayerVars.player_health) + "/" + str(PlayerVars.player_max_health)
