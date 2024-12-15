a2f3fce2998f915ca6edbdfb1d835fc72eb060b8
Justin Montgomery <tryfinsigabrt@yahoo.com>
2024-12-15 10:07:02 -0600

    Show message to player when there is 1 enemy left and auto-hide with timer/end condition

--- Levels/CriticalDangerFeedback.gd
+++ Levels/CriticalDangerFeedback.gd
@@ -1,15 +1,31 @@
 class_name CriticalDanagerFeedback extends Node
 
+const LastEnemyMessage = "Last Enemy!"
+const LowHealthMessage = "Health Low!"
+const TimeRunningOutMessge = "Hurry Up!"
+
 # TODO: Add "taking too long" feedback
 # TODO: Add low player health feedback
 
 # RoundManager is the owner of win conditions
 @onready var roundManager = get_tree().current_scene.get_node("EveryLevelReusedStuff")
+@onready var feedback_text = $FeedbackText
+@onready var timer = $FeedbackTimer
+
+@export var message_display_time_secs = 2
 
 # Called when the node enters the scene tree for the first time.
 func _ready():
-	pass # Replace with function body.
-
+	if is_instance_valid(feedback_text):
+		feedback_text.visible = false
+	else:
+		push_error("CriticalDanagerFeedback: feedback_text not in scene!")
+	
+	if is_instance_valid(timer):
+		timer.wait_time = message_display_time_secs
+		timer.connect("timeout", Callable(self, "on_timer_expired"))
+	else:
+		push_error("CriticalDanagerFeedback: timer not in scene!")
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta):
@@ -17,8 +33,38 @@ func _process(delta):
 
 func on_enemy_defeated():
 	# print("CriticalDanagerFeedback: Enemy Defeated")
+	var enemiesRemaining = roundManager.level_goal - roundManager.defeated_enemies
 
 	# Check if it is the last enemy and if so will show message to player
-	# TODO: Add message like "A: Wading in" on screen
-	if roundManager.level_goal - roundManager.defeated_enemies == 1:
-		print("LAST ENEMY!")
+	if enemiesRemaining == 1:
+		show_message(LastEnemyMessage)
+	elif enemiesRemaining == 0:
+		# Hide any messages as victory text will display
+		force_hide_message()
+		
+func show_message(text):
+	print("CriticalDanagerFeedback: " + text)
+	# set the text of the feedback text
+	if feedback_text == null:
+		return
+	
+	feedback_text.text = text
+	feedback_text.visible = true
+	if timer:
+		timer.start()
+		
+	# TODO: Fade out and animate the message up the screen
+	# or some other text animation like changing size or wiggling
+	
+func force_hide_message():
+	hide_message()
+	if timer:
+		timer.stop()
+		
+func on_timer_expired():
+	print("CriticalDanagerFeedback: Timer expired")
+	hide_message()
+
+func hide_message():
+	if feedback_text:
+		feedback_text.visible = false
