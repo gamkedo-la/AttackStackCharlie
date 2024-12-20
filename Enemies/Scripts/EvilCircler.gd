3a698504308c0d7058789c798e5cb93c0ded0715
Justin Montgomery <tryfinsigabrt@yahoo.com>
2024-12-20 13:43:19 -0600

    Fix EvilCircler teleporting issue when switching from seek to orbit behavior
    - Subtle difference between atan2 quadrants and how vector angle computes them. Using same polar <-> cartesian coordinates angle calculation method between seek and circling

--- Enemies/Scripts/EvilCircler.gd
+++ Enemies/Scripts/EvilCircler.gd
@@ -5,35 +5,40 @@ const ROTATE_SPEED_MAX = 0.245
 const CIRCLE_DISTANCE_MAX = 230
 const CIRCLE_DISTANCE_MIN = 100
 
+# TODO: Add an "arrive at" to slow down as get on the circle go from 5x down to regular speed that we orbit
+
 var circlePercent = 0
 var myOrbitDist = 0
 var myOrbitRate = 0
 var inRangeOfTarget = false
 var cwOrbit = false;
+var rotDir = 0
 
 func _ready():
 	super._ready() # run the parent enemy init
 	cwOrbit = randi_range(0,10) < 5
 	myOrbitDist = randf_range(CIRCLE_DISTANCE_MIN,CIRCLE_DISTANCE_MAX)
 	myOrbitRate = randf_range(ROTATE_SPEED_MIN,ROTATE_SPEED_MAX)
+	rotDir = -1 if cwOrbit else 1
 
 func _process(delta):
-	var rotationAmount = circlePercent * TAU
+	#print("global_position=" + str(global_position) + "circlePercent=" + str(circlePercent) + ";rotation=" + str(rotation))
+	
 	var distToPlayer = global_position.distance_to(targetPos)
 	var wasPos = global_position;
 	
 	if inRangeOfTarget || distToPlayer <= myOrbitDist:
 		if inRangeOfTarget == false:
 			inRangeOfTarget = true
-			var targetSpotOnUnitCircle = (global_position - targetPos).normalized()
-			var initialAngle = targetSpotOnUnitCircle.angle()
+			# We are arriving at the target and want to start orbiting from the current position
+			# Convert from cartesian to polar coordinates which just involves calculating the central angle
+			var initialAngle = atan2(global_position.y-targetPos.y, global_position.x-targetPos.x)
 			circlePercent = initialAngle / TAU
-			if cwOrbit:
-				circlePercent *= -1
-		var rotDir = -1;
-		if cwOrbit:
-			rotDir = 1
+			return
+		
 		circlePercent += myOrbitRate * delta * rotDir
+		var rotationAmount = circlePercent * TAU
+
 		global_position.x = targetPos.x + cos(rotationAmount) * myOrbitDist
 		global_position.y = targetPos.y + sin(rotationAmount) * myOrbitDist
 		rotation = rotationAmount + rotDir * PI/2
