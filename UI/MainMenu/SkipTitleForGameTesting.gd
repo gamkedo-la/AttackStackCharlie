2bd9253493dcdefb4e66325f133df881769c8fdb
Dan Dela Rosa <dandelarosadeveloper@gmail.com>
2025-01-04 17:52:17 -0500

    Import and play animated logo

--- UI/MainMenu/SkipTitleForGameTesting.gd
+++ UI/MainMenu/SkipTitleForGameTesting.gd
@@ -12,3 +12,6 @@ func _ready():
 	if skipIntroForFasterTesting:
 		print("SPLASH/MENU BYPASSED to test - skipIntroForFasterTesting see top of UI/MainMenu/main_menu.tscn")
 		SceneManager.SwitchScene("TestLevelA")
+
+func _on_intro_sequence_animation_finished(anim_name):
+	$BoxContainer/Control/AnimatedLogo.play()
