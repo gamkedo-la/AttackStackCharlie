f65c5fb00bdd65c93dbfe8ae1974a4f2259820bd
Justin Montgomery <tryfinsigabrt@yahoo.com>
2025-01-17 20:14:48 -0600

    Update asteroid logic to randomize movement and warp to other side of screen when leaving boundary

--- Enemies/Asteroid.gd
+++ Enemies/Asteroid.gd
@@ -1,11 +1,28 @@
 extends CharacterBody2D
 
-var rotation_speed = 90
-@export var speed: float = 100.0
+@export var rotation_speed_min = 45
+@export var rotation_speed_max = 135
+
+@export var speed_min: float = 50.0
+@export var speed_max: float = 150.0
+
+@export var heading_deviation_min = 10
+@export var heading_deviation_max = 30
+
+var speed : float
+var rotation_speed: float
+var heading : Vector2
 
 var camera_width: float 
-var camera_height: float 
+var camera_height: float
 
+var topY: float
+var bottomY: float
+var leftX: float
+var rightX: float
+
+# Used to detect when the asteroid leaves the screen
+@onready var areaBoundary = get_tree().current_scene.get_node("EveryLevelReusedStuff/Boundaries/BoundaryCollision")
 
 @export var player: Node2D
 func _ready():
@@ -13,27 +30,65 @@ func _ready():
 	camera_width = viewport.size.x 
 	camera_height = viewport.size.y
 	
-	if randf_range(0, 100) < 50:
-		speed = -speed
-	
-	# position.x = -50
-	# position.y = randf_range(0, camera_height)
-	var sprite = $AnimatedSprite2D
+	if is_instance_valid(areaBoundary):
+		areaBoundary.connect("body_exited", Callable(self, "on_exited_level_bounds"))
+	else:
+		push_error("Asteroid: Missing Level area Boundary!")
 
+	topY = 10
+	bottomY = camera_height - 10
+	leftX = 10
+	rightX = camera_width - 10
+
+	init_movement()
+
+func init_movement():
+	speed = randf_range(speed_min, speed_max)
+	rotation_speed = randf_range(rotation_speed_min, rotation_speed_max)
+
+	# randomize heading
+	heading = Vector2(randf_range(-1, 1), randf_range(-1, 1)).normalized()
+	
+func perturb_heading():
+	var angle = randf_range(10, 30) * ( 1 if randf() <= 0.5 else -1)
+	# rotate heading by angle
+	heading = heading.rotated(deg_to_rad(angle))
+	
+	#print("Changed heading")
+	
 func _process(delta):
 	rotation_degrees += rotation_speed * delta
-	position.x += speed * delta
-	
-	if position.x < 10:
-		respawn_on_right()
-	if position.x > camera_width - 10:
-		respawn_on_left()
-	
-	
-func respawn_on_left():
-	position.x = 15
-	position.y = randf_range(0, camera_height)
+	position += speed * heading * delta
 
-func respawn_on_right():
-	position.x = camera_width - 15
-	position.y = randf_range(0, camera_height)
+func on_exited_level_bounds(body):
+	# This gets called for every body leaving the area. Need to filter to self
+	# Maybe there is a better way to filter the notifications?
+	if body != self:
+		return
+		
+	# Want to reflect the position of the asteroid so that if it was on bottom re-appears on the top a
+	# if on the left, reappears on right going in opposite direction
+
+	#print("Asteroid - " + to_string() +
+	 #" exited screen: pos={" + str(position.x) + "," + str(position.y) + "};" +
+	 #" heading={" + str(heading.x) + "," + str(heading.y) +"}")
+	
+	if global_position.x <= leftX:
+		global_position.x = rightX
+	elif global_position.x >= camera_width - 10:
+		global_position.x = leftX
+		
+	if global_position.y <= topY:
+		global_position.y = bottomY
+	elif global_position.y >= camera_height - 10:
+		global_position.y = topY
+	
+	# randomize the movement when it comes out the other end
+	# Not sure I like fully randomzing it again
+	# init_movement()
+	# Instead just perturb the heading slightly so it isn't super repetitive
+	perturb_heading()
+	
+	#print("Asteroid - " + to_string() +
+	 #" re-appear at pos={" + str(position.x) + "," + str(position.y) + "};" +
+	 #" heading={" + str(heading.x) + "," + str(heading.y) +"}")
