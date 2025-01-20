9004e0a77dd729dfdcb19f2db08b206e657be334
Justin Montgomery <tryfinsigabrt@yahoo.com>
2025-01-20 17:21:25 -0600

    Refactor CRT effect to its own scene and instance it in EveryLevelReusedStuff

--- Shaders/CRTEffect.gd
+++ Shaders/CRTEffect.gd
@@ -1,9 +1,23 @@
-extends Sprite2D
+extends CanvasLayer
 
-@export var enable_crt: bool = true;
+@onready var sprite = $CRTEffect
+
+@export var enable_crt: bool = true
+
+var original_material
 
 func _ready():
-	if !enable_crt:
-		print("Disabling CRT effect");
-		material = null
-		visible = false
+	original_material = sprite.material
+	
+	toggle_crt(enable_crt)
+
+func toggle_crt(enable):
+	var material = original_material if enable else null
+	
+	sprite.material = material
+	sprite.visible = enable
+	
+	enable_crt = enable
+	
+	if !enable:
+		print("Disabling CRT effect")
