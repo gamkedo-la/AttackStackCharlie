8565698011a162fed1fb47be65abd173def5895c
Chris DeLeon <chris@gamkedo.com>
2025-01-26 15:38:37 -0500

    Invul sound added, earlier levels tuned to run longer

--- Powerups/Invincibility.gd
+++ Powerups/Invincibility.gd
@@ -1,9 +1,18 @@
 extends "res://Powerups/Powerup.gd"
-var invincibility_time: float = 5; # 5 seconds of invincibility
+var invincibility_time: float = 5 # 5 seconds of invincibility
+var audio_player
 
+func _ready():
+	audio_player = $AudioStreamPlayer
+	audio_player.connect("finished", Callable(self, "_on_AudioStreamPlayer_finished"))
+	
 func collected(body):
 	if body.has_method("upgradeInvincibility"):
 		body.upgradeInvincibility(invincibility_time);
-		queue_free();
+		# queue_free();
+		audio_player.play()
 		return true;
 	return false
+
+func _on_AudioStreamPlayer_finished():
+	queue_free()
